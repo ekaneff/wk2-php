c513ece1b10bf86ae262f63457be463b5371f0bf
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-08 22:22:54 -0500

    added footer text

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -18,7 +18,7 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
-			<h1>THIS IS WORKING</h1>
+			<h1>This is definitely working</h1>
 				<?php
 				get_template_part( 'template-parts/footer/footer', 'widgets' );
 
